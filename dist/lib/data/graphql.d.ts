1b782904b1be07d14ee72d6e16051b93b5cee9b8
Cuong Pham-Cong <congcuong.cse@gmail.com>
2020-01-04 12:04:35 +0700

    Published by publish-to-git fe716629bb35aab6b532886f278a917717e70f6f (HEAD -> refs/heads/master, tag: refs/tags/v0.1.2, refs/remotes/origin/master, refs/remotes/origin/HEAD) chore(release): 0.1.2

---
+++ dist/lib/data/graphql.d.ts
@@ -0,0 +1,20 @@
+import { ApolloClient } from 'apollo-client';
+import { DocumentNode } from 'graphql';
+import { JSONSchema7 } from 'json-schema';
+export interface FrontierDataGraphQLProps {
+    mutation: DocumentNode;
+    client?: ApolloClient<any>;
+    schema?: JSONSchema7;
+    save?: (values: object) => Promise<undefined | object>;
+    formats?: {
+        [k: string]: string;
+    };
+}
+export declare type SchemaFromGraphQLPropsReturn = {
+    schema: JSONSchema7;
+    mutationName: string;
+} | null;
+export declare function schemaFromGraphQLProps(props: FrontierDataGraphQLProps): Promise<SchemaFromGraphQLPropsReturn>;
+export declare function saveData(props: FrontierDataGraphQLProps, values: object): Promise<undefined | object>;
+export declare function getMutationNameFromDocumentNode(mutation: DocumentNode): string | null;
+export declare function buildFormSchema(schema: JSONSchema7, mutationName: string): JSONSchema7;
